38f4f029ff7f15a4cccf43fbf9d6fd310c65e275
codemercenary <jsanchez@leapmotion.com>
2013-08-05 05:28:05 -0700

    Using token duplication in an effort to turn off MIC for the current process

--- Testbed/Testbed.cpp
+++ Testbed/Testbed.cpp
@@ -26,35 +26,19 @@ typedef NTSTATUS (*t_ZwUserSendTouchInput)(HWND hWnd, int rdx, TouchData* touchD
 HANDLER_FUNCTION_EX HandlerProc;
 SERVICE_MAIN_FUNCTIONW Run;
 
+HANDLE token;
 auto hmod = LoadLibrary(L"user32");
 auto procAddr = (t_ZwUserSendTouchInput)GetProcAddress(hmod, LPCSTR(1500));
-auto hService = RegisterServiceCtrlHandlerExW(L"Testbed", HandlerProc, nullptr);
 SERVICE_TABLE_ENTRY gc_table[] = {{L"Testbed", Run}, {nullptr, nullptr}};
+
+// Impersonation token, used to turn off MIC:
+HANDLE hImpersonation;
 
 DWORD __stdcall HandlerProc(DWORD dwControl, DWORD dwEventType, void* lpEventData, void* lpContext) {
   return 0;
 }
 
-void __stdcall Run(DWORD dwNumServicesArgs, LPWSTR *lpServiceArgVectors) {
-  DWORD rs;
-  if(!procAddr)
-    throw std::runtime_error("Cannot find touch input entrypoint");
-
-  HANDLE token;
-  if(!OpenProcessToken(GetCurrentProcess(), GENERIC_ALL, &token))
-    throw std::runtime_error("Could not open process token");
-
-  // Verify that our mandatory policy token is turned off:
-  TOKEN_MANDATORY_POLICY policy;
-  GetTokenInformation(token, TokenMandatoryPolicy, &policy, sizeof(policy), &rs);
-
-  if(policy.Policy) {
-    OutputDebugString(L"Mandatory policy flag was nonzero\n");
-    return;
-  }
-
-  CloseHandle(token);
-
+void TestFireSingleTouchInput(void) {
   HWND hWnd = GetDesktopWindow();
   TouchData data;
   data.a1 = 0x00017609;
@@ -74,10 +58,103 @@ void __stdcall Run(DWORD dwNumServicesArgs, LPWSTR *lpServiceArgVectors) {
        << GetLastError() << endl;
 }
 
-int main(int argc, char* argv[]) {
-  OutputDebugString(L"Test test\n");
-  StartServiceCtrlDispatcher(gc_table);
+/// <summary>
+/// Turns off mandatory policy on the current process token
+/// </summary>
+void DisableMIC(void) {
+  __debugbreak();
 
+  DWORD rs = 0;
+  LUID relabel;
+  LookupPrivilegeValue(nullptr, SE_RELABEL_NAME, &relabel);
+
+  // Adjust privileges on ourselves first:
+  TOKEN_PRIVILEGES tkp;
+  tkp.PrivilegeCount = 1;
+  tkp.Privileges[0].Luid = relabel;
+  tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+  // Grant ourselves the ability to modify our own MIC:
+  AdjustTokenPrivileges(token, false, &tkp, 0, nullptr, nullptr);
+
+  // Duplicate our token:
+  DuplicateTokenEx(token, TOKEN_ALL_ACCESS, nullptr, SecurityImpersonation, TokenImpersonation, &hImpersonation);
+
+  // Switch off mandatory policy:
+  TOKEN_MANDATORY_POLICY policy;
+  policy.Policy = 0;
+  SetTokenInformation(hImpersonation, TokenMandatoryPolicy, &policy, sizeof(policy));
+
+  // Flip over to our impersonation token:
+  SetThreadToken(nullptr, token);
+}
+
+void RunProxy(void) {
+  DWORD rs;
+
+  // Disable MIC first:
+  DisableMIC();
+
+  // Validate that MIC was disabled:
+  vector<char> buf(0x100);
+  auto& lbl = (TOKEN_MANDATORY_LABEL&)buf[0];
+  GetTokenInformation(token, TokenIntegrityLevel, &lbl, (DWORD)buf.size(), &rs);
+
+  DWORD subAuthority = *GetSidSubAuthority(lbl.Label.Sid, 0);
+
+  // Verify that our mandatory policy token is turned off:
+  TOKEN_MANDATORY_POLICY policy;
+  GetTokenInformation(token, TokenMandatoryPolicy, &policy, sizeof(policy), &rs);
+
+  if(policy.Policy) {
+    OutputDebugString(L"Mandatory policy flag was nonzero\n");
+    return;
+  }
+
+  __debugbreak();
+  CloseHandle(token);
+}
+
+void __stdcall Run(DWORD dwNumServicesArgs, LPWSTR *lpServiceArgVectors) {
+  SERVICE_STATUS status;
+  status.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+  status.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+  status.dwWin32ExitCode = 0;
+  status.dwServiceSpecificExitCode = 0;
+  status.dwCheckPoint = 0;
+  status.dwWaitHint = 0;
+
+  // Okay, obtain a handle:
+  auto hService = RegisterServiceCtrlHandlerExW(L"Testbed", HandlerProc, nullptr);
+  
+  // Running now:
+  status.dwCurrentState = SERVICE_RUNNING;
+  SetServiceStatus(hService, &status);
+
+  RunProxy();
+
+  status.dwCurrentState = SERVICE_STOPPED;
+  SetServiceStatus(hService, &status);
+}
+
+int main(int argc, char* argv[]) {
+  if(!procAddr)
+    throw std::runtime_error("Cannot find touch input entrypoint");
+
+  if(!OpenProcessToken(GetCurrentProcess(), TOKEN_ALL_ACCESS, &token))
+    throw std::runtime_error("Could not open process token");
+
+  // Decide whether to run in service mode or interactive mode:
+  DWORD sessionID;
+  ProcessIdToSessionId(GetCurrentProcessId(), &sessionID);
+  if(sessionID)
+    RunProxy();
+  else {
+    OutputDebugString(L"Test test\n");
+    StartServiceCtrlDispatcher(gc_table);
+  }
+
+  CloseHandle(token);
 	return 0;
 }
 
